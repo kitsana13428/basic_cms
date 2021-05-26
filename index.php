017a4aa58ac3b508d929f4f1d189a1e9b1e98da8
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-05-26 15:06:27 +0700

    add file
    add file cms

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Page</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+        <header>
+            <div class="container">
+                <div class="logo">
+                    <h1><a href="index.php">KitsanaCMS</a></h1>
+                </div>
+           
+
+                    <form action="search.php" method="get" enctype="multipart/form-data">
+                        <input type="text" name="value" placeholder="search topic" size="25">
+                        <input type="submit">
+                    </form>
+                </div>
+        </header>
+
+        <section class="content">
+            <div class="container"> 
+                <figure>
+                    <h1><a href="#">This is post title</a></h1>
+                    <img width="640" height="360" src="img/001.jpg" alt="">
+                    <figcaption>
+                        <p>Posted By <strong>Admin</strong>| Published on <strong>2021-05-26</strong></p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt est, aut molestias maxime natus quod, beatae cum nostrum, atque sunt voluptatibus. Nihil accusamus architecto totam cupiditate optio eum eligendi nesciunt.</p>
+                        <a href="#">Read more</a>
+                    </figcaption>
+                </figure>
+                <figure>
+                    <h1><a href="#">This is post title</a></h1>
+                    <img width="640" height="360" src="img/001.jpg" alt="">
+                    <figcaption>
+                        <p>Posted By <strong>Admin</strong>| Published on <strong>2021-05-26</strong></p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt est, aut molestias maxime natus quod, beatae cum nostrum, atque sunt voluptatibus. Nihil accusamus architecto totam cupiditate optio eum eligendi nesciunt.</p>
+                        <a href="#">Read more</a>
+                    </figcaption>
+                </figure>
+                <figure>
+                    <h1><a href="#">This is post title</a></h1>
+                    <img width="640" height="360" src="img/001.jpg" alt="">
+                    <figcaption>
+                        <p>Posted By <strong>Admin</strong>| Published on <strong>2021-05-26</strong></p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt est, aut molestias maxime natus quod, beatae cum nostrum, atque sunt voluptatibus. Nihil accusamus architecto totam cupiditate optio eum eligendi nesciunt.</p>
+                        <a href="#">Read more</a>
+                    </figcaption>
+                </figure>
+            </div> 
+        </section>    
+</body>
+</html>
